065e0c500edf0a027b455d35391a0a5b3adf4f80
donkirkby <donkirkby@5b1e2516-15c8-11df-a35d-4d785ab5a681>
2010-10-01 17:06:17 +0000

    Add Reminder project.

---
+++ trunk/Reminder/Reminder/Reminder/Window1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Reminder
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            string title = "Time to stretch";
+            string text = "Get out of your chair, you lazy bum!";
+
+            MyNotifyIcon.ShowBalloonTip(title, text, MyNotifyIcon.Icon);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(1, 0, 0);
+            timer.Start();
+        }
+    }
+}
