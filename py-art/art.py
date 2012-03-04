c6183b7b27fac6538eb87777a825ceea8a495a92
donkirkby@gmail.com <donkirkby@gmail.com@5b1e2516-15c8-11df-a35d-4d785ab5a681>
2012-03-04 02:56:17 +0000

    Add 3x2 layout.

--- py-art/art.py
+++ py-art/art.py
@@ -33,21 +33,43 @@ def display_square(size, display, curve):
     display.add_point((size/2 - 1, 0))
     display.close_curve()
 
+def display_3x2(size, display, curve):
+    display.add_point((-1, -1))
+    display.add_point((-1, 2*size))
+    display.add_point((3*size, 2*size))
+    display.add_point((3*size, -1))
+    display.close_curve()
+    
+    curve.draw_cell((0, size - 1), (size - 1, size - 1), -1)
+    display.add_point((size, size-1))
+    curve.draw_cell((size, size-1), (2*size - 1, size - 1), -1)
+    display.add_point((2*size, size - 1))
+    curve.draw_cell((2*size, size - 1), (3*size - 1, size - 1), -1)
+    display.add_point((3*size - 1, size))
+    curve.draw_cell((3*size - 1, size), (2*size, size), -1)
+    display.add_point((2*size - 1, size))
+    curve.draw_cell((2*size - 1, size), (size, size), -1)
+    display.add_point((size - 1, size))
+    curve.draw_cell((size - 1, size), (0, size), -1)
+    display.add_point((0, size - 1))
+    display.close_curve()
+
 filename = 'input/bwR.jpg'
 filename = '/home/don/Dropbox/Art/balcony.jpg'
 filename = 'input/balcony-cropped-levels.jpg'
+filename = 'input/balcony-cropped-3x2.jpg'
 image = Image.open(filename)
 
-size = 512
-display = SvgDisplay('output/balcony.svg', 2*size + 2, size + 2)
+size = 256
+display = SvgDisplay('output/balcony3x2.svg', 3*size + 2, 2*size + 2)
 display.offset = (1,1)
 curve = Curve(display, image)
 curve.scale = (1, 1)
-curve.calculate_levels(6, 0, 0, 2*size, size)
+curve.calculate_levels(4, 0, 0, 3*size, 2*size)
 
 print "Starting."
 
-display_rectangle(size, display, curve)
+display_3x2(size, display, curve)
 
 display.close()
 print "Success."
