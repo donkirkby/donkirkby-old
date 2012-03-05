98a8abfa5c7056fcfd0a8fa0ce172c21b777c9b2
donkirkby@gmail.com <donkirkby@gmail.com@5b1e2516-15c8-11df-a35d-4d785ab5a681>
2012-03-05 05:24:29 +0000

    add border to main path and add margin.

--- trunk/py-art/art.py
+++ trunk/py-art/art.py
@@ -54,23 +54,46 @@ def display_3x2(size, display, curve):
     display.add_point((0, size - 1))
     display.close_curve()
 
+def display_3x2_with_border(size, display, curve):
+    display.add_point((-1, size))
+    display.add_point((-1, 2*size))
+    display.add_point((3*size, 2*size))
+    display.add_point((3*size, -1))
+    display.add_point((-1, -1))
+    display.add_point((-1, size - 1))
+    
+    display.add_point((0, size - 1))
+    curve.draw_cell((0, size - 1), (size - 1, size - 1), 1)
+    display.add_point((size, size-1))
+    curve.draw_cell((size, size-1), (2*size - 1, size - 1), 1)
+    display.add_point((2*size, size - 1))
+    curve.draw_cell((2*size, size - 1), (3*size - 1, size - 1), 1)
+    display.add_point((3*size - 1, size))
+    curve.draw_cell((3*size - 1, size), (2*size, size), 1)
+    display.add_point((2*size - 1, size))
+    curve.draw_cell((2*size - 1, size), (size, size), 1)
+    display.add_point((size - 1, size))
+    curve.draw_cell((size - 1, size), (0, size), 1)
+    display.close_curve()
+
 filename = 'input/bwR.jpg'
 filename = '/home/don/Dropbox/Art/balcony.jpg'
 filename = 'input/balcony-cropped-levels.jpg'
 filename = 'input/balcony-cropped-3x2.jpg'
 filename = 'input/Kirkby-trimmed.jpg'
+filename = 'input/lena-cropped.jpg'
 image = Image.open(filename)
 
 size = 256
-display = SvgDisplay('output/Kirkby.svg', 3*size + 2, 2*size + 2)
-display.offset = (1,1)
+display = SvgDisplay('output/lena.svg', 3*size + 22, 2*size + 22)
+display.offset = (11,11)
 curve = Curve(display, image)
 curve.scale = (1, 1)
 curve.calculate_levels(4, 0, 0, 3*size, 2*size)
 
 print "Starting."
 
-display_3x2(size, display, curve)
+display_3x2_with_border(size, display, curve)
 
 display.close()
 print "Success."
