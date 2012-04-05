aa073457fd70aff13ff110c90a27001fd51d92cb
donkirkby@gmail.com <donkirkby@gmail.com@5b1e2516-15c8-11df-a35d-4d785ab5a681>
2012-04-05 05:49:42 +0000

    fix warnings in py-art/turtle_display.py

--- py-art/turtle_display.py
+++ py-art/turtle_display.py
@@ -1,24 +1,29 @@
-import turtle
+from turtle import Turtle
 
 class TurtleDisplay:
     __start_point = None
     __scale = (2, -2)
     __offset = (-250, 250)
     
+    def __init__(self):
+        self.__turtle = Turtle()
+    
     def add_point(self, point):
         x, y = self.translate(point)
         if self.__start_point:
-            turtle.goto(x, y) #@UndefinedVariable
+            self.__turtle.goto(x, y)
         else:
-            turtle.speed(0) #@UndefinedVariable
+#            self.__turtle.speed(0)
+            self.__turtle.tracer(100)
             self.__start_point = point
-            turtle.penup() #@UndefinedVariable
-            turtle.goto(x, y) #@UndefinedVariable
-            turtle.pendown() #@UndefinedVariable
+            self.__turtle.penup()
+            self.__turtle.goto(x, y)
+            self.__turtle.pendown()
 
     def close_curve(self):
         x, y = self.translate(self.__start_point)
-        turtle.goto(x, y) #@UndefinedVariable
+        self.__turtle.tracer(1)
+        self.__turtle.goto(x, y)
         self.__start_point = None
         
     def close(self):
